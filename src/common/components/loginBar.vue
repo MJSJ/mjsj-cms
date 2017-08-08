459191b6a7f886979891115fcd29a39e82479f92
yursile <kuangyu02@meituan.com>
2017-08-08 21:50:00 +0800

    format

--- src/common/components/loginBar.vue
+++ src/common/components/loginBar.vue
@@ -1,39 +1,39 @@
 <template>
-<el-menu theme="dark" mode="horizontal">
-  <el-col :span="2">
-    <el-menu-item class="top-logo" index="1">MJSJ</el-menu-item>
-  </el-col>
-  <el-col :span="22">
-    <el-row type="flex" justify="space-between">
-      <el-row type="flex" justify="start">
-      </el-row>
-      <el-submenu index="10" >
-        <template slot="title">{{name}}</template>
-        <el-menu-item index="logout" @click="logout">登出</el-menu-item>
-      </el-submenu>
-    </el-row>
-  </el-col>
-</el-menu>
+    <el-menu theme="dark" mode="horizontal">
+        <el-col :span="2">
+            <el-menu-item class="top-logo" index="1">MJSJ</el-menu-item>
+        </el-col>
+        <el-col :span="22">
+            <el-row type="flex" justify="space-between">
+                <el-row type="flex" justify="start">
+                </el-row>
+                <el-submenu index="10">
+                    <template slot="title">{{name}}</template>
+                    <el-menu-item index="logout" @click="logout">登出</el-menu-item>
+                </el-submenu>
+            </el-row>
+        </el-col>
+    </el-menu>
 </template>
 
 <script>
 let DATA = window.__DATA || {};
 export default {
-  data(){
-    return {
-        name:DATA.name || ""
+    data() {
+        return {
+            name: DATA.name || ""
+        }
+    },
+    methods: {
+        logout() {
+            location.href = "/page/logout";
+        }
     }
-  },
-  methods:{
-      logout(){
-          location.href = "/page/logout";
-      }
-  }
 }
 </script>
 <style lang="less">
-.top-logo{
-    color:#fff!important;
+.top-logo {
+    color: #fff!important;
     font-size: 26px!important;
     cursor: auto!important;
 }
--- src/index/components/subjectList.vue
+++ src/index/components/subjectList.vue
@@ -1,117 +1,109 @@
 <template>
-<div class="subject_list">
-    <el-table
-      border
-      :data="subjectList"
-      style="width: 100%">
-
-      <el-table-column
-      prop="id"
-      label="ID"
-      width="120">
-    </el-table-column>
-
-        <el-table-column
-      prop="name"
-      label="名称"
-      width="180">
-    </el-table-column>
-
-       <el-table-column
-        label="操作"
-        width="180">
-        <template scope="scope">
-            <el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-            <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-        </template>
-      </el-table-column>
-
-   
-     <el-table-column
-        label="上次编辑"
-        width="300">
-        <template scope="scope">
-            <div slot="reference">
-                {{scope.row.lastEdit.userName}}--{{scope.row.lastEdit.time}}
-            </div>
-        </template>
-      </el-table-column>
-
-    </el-table>
-</div>
+    <div class="subject_list">
+        <el-table border :data="subjectList" style="width: 100%">
+    
+            <el-table-column prop="id" label="ID" width="120">
+            </el-table-column>
+    
+            <el-table-column prop="name" label="名称" width="180">
+            </el-table-column>
+    
+            <el-table-column label="操作" width="180">
+                <template scope="scope">
+                    <el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                    <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                </template>
+            </el-table-column>
+    
+            <el-table-column label="上次编辑" width="300">
+                <template scope="scope">
+                    <div slot="reference">
+                        {{scope.row.lastEdit.userName}}--{{scope.row.lastEdit.time}}
+                    </div>
+                </template>
+            </el-table-column>
+    
+        </el-table>
+    </div>
 </template>
 
 <script>
-import {SUBJECT_ITEM} from "../../common/api/url.js"
+import { SUBJECT_ITEM } from "../../common/api/url.js"
 
 export default {
     data() {
-       return {
+        return {
         }
     },
-    props:{
-        subjectList:{}
+    props: {
+        subjectList: {}
     },
-    methods:{
+    methods: {
         handleEdit(index, row) {
-            window.location.href = SUBJECT_ITEM + "?subjectId="+row.id;
+            window.location.href = SUBJECT_ITEM + "?subjectId=" + row.id;
         },
         handleDelete(index, row) {
             this.$confirm('确认删除？')
                 .then(_ => {
                     console.log(index, row);
-                    this.$store.dispatch("deleteSubject",{subjectID:row.id})
+                    this.$store.dispatch("deleteSubject", { subjectID: row.id })
                 })
                 .catch(_ => { });
         }
     },
-    mounted(){
-        
+    mounted() {
+
     }
-   
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" rel="stylesheet/less">
-
-.subject_list{
+.subject_list {
     max-width: 780px;
 }
-.el-form-item__content{
+
+.el-form-item__content {
     text-align: left;
 }
-.time_to{
+
+.time_to {
     text-align: center;
 }
-.time_sync{
-    margin-left:1em;
+
+.time_sync {
+    margin-left: 1em;
 }
-.product_confirm{
-    margin-left:1em;
+
+.product_confirm {
+    margin-left: 1em;
 }
-.baseinfo{
-    margin-top:20px;
+
+.baseinfo {
+    margin-top: 20px;
 }
-.s_item{
-    &:after{
-        content:"、";
-        display:inline-block;
+
+.s_item {
+    &:after {
+        content: "、";
+        display: inline-block;
     }
-    &:last-child{
-        &:after{
-            content:"";
+    &:last-child {
+        &:after {
+            content: "";
         }
     }
 }
-.semi{
-    &:after{
-        content:";";
-        display:inline-block;
+
+.semi {
+    &:after {
+        content: ";";
+        display: inline-block;
     }
-    &:last-child{
-        &:after{
-            content:"";
+    &:last-child {
+        &:after {
+            content: "";
         }
     }
 }
