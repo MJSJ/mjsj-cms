eefd88b7d8cadea9ff593e78efead85f507f0cc6
Leibown <leibown.leila@gmail.com>
2017-09-03 17:26:52 +0800

    本地该网络接口

--- src/common/api/api.js
+++ src/common/api/api.js
@@ -2,10 +2,11 @@ import * as db from "./db.js"
 
 import {get, all, local} from "./tool.js"
 
-const HOST = "mykrystal.com"
+const HOST = "//mykrystal.com"
 const GET_URLS = {
     // fetchSubject:HOST+"/cms/fetchSubjectList"
     // getShopBoard:HOST+"/joy/merchant/getshopboard",
+    fetchUserList: HOST + "/cms/fetchUserList",
 }
 const POST_URLS = {
     // newUploadImage:HOST+"/joy/merchant/newuploadimage",
@@ -17,16 +18,16 @@ const JSONP_URLS = {
 
 //本地Mock数据
 const LOCAL_URLS = {
-    fetchSubjectList: db.subjectList,
-    deleteSubject: db.result,
-    fetchSubject: db.subject,
-    fetchUserList: db.userList,
-    fetchTotalTopics: db.totalTopics
+    // fetchSubjectList: db.subjectList,
+    // deleteSubject: db.result,
+    // fetchSubject: db.subject,
+    // fetchUserList: db.userList,
+    // fetchTotalTopics: db.totalTopics
 }
 
 const api = {
     ...all(GET_URLS, POST_URLS, JSONP_URLS),
-    ...local(LOCAL_URLS)//将请求mock到本地，有线上接口后注释本行
+    // ...local(LOCAL_URLS)//将请求mock到本地，有线上接口后注释本行
 }
 
 export default api
