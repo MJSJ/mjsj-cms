3440fe70c1444b9123c6d9da050e37590a4667a0
yursile <kuangyu02@meituan.com>
2017-09-15 00:28:58 +0800

    fix subject

--- src/index/app.vue
+++ src/index/app.vue
@@ -64,7 +64,7 @@ export default {
         // })
         // .then((data)=>{
         //     this.$store.dispatch('fetchUserList');
-        //     this.$store.dispatch('fetchTotalTopics');
+        //     this.$store.dispatch('fetchSubjectList');
         // })
 
 
