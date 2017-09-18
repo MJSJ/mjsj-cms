8310ba3e7f29285ebeff93fa5aa088bfa2ade8cf
yursile <kuangyu02@meituan.com>
2017-09-18 23:40:33 +0800

    add file

--- src/common/api/api.js
+++ src/common/api/api.js
@@ -1,8 +1,9 @@
 import * as db from "./db.js"
 
 import {get, all, local} from "./tool.js"
+import axios from "axios"
 
-const HOST = "//mykrystal.com"
+export const HOST = "//mykrystal.com"
 //const HOST = "//localhost:8081"
 const GET_URLS = {
     fetchSubjectList:HOST+"/cms/fetchSubjectList",
@@ -25,6 +26,8 @@ const POST_URLS = {
     deleteUser: HOST + "/cms/deleteUser",
 }
 
+const UPLOAD_FILE_URL = HOST+"/batchUpload"
+
 const JSONP_URLS = {
 
 }
@@ -38,9 +41,33 @@ const LOCAL_URLS = {
     // fetchTotalTopics: db.totalTopics
 }
 
+function uploadFile(file){
+    var formData = new FormData();
+    formData.append('file', file);
+    return new Promise((resolve,reject)=>{
+        axios({
+            method: 'post',
+            url: UPLOAD_FILE_URL,
+            data: formData,
+            withCredentials: true
+        })
+        .then((res)=>{
+            return res.data
+        })
+        .then((data)=>{
+            resolve(data.data)
+        })
+        .catch(e=>{
+            console.error(e)
+        })
+    })
+}
+
 const api = {
     ...all(GET_URLS, POST_URLS, JSONP_URLS),
+    uploadFile,
     //...local(LOCAL_URLS)//将请求mock到本地，有线上接口后注释本行
 }
+
 
 export default api
--- src/index/components/system/file.vue
+++ src/index/components/system/file.vue
@@ -1,18 +1,112 @@
 <template>
-    <div>
-        文件上传模块，需求待讨论...
+    <div class="file_container">
+        <div class="file-uploader" :style="hasSelectedFile?'border:none':''" @click="selectFile">
+            <el-progress type="circle" v-if="hasSelectedFile" :percentage="percentage"></el-progress>
+            <div v-else class="file-uploader-icon el-icon-plus"></div>
+        </div>
+
+        <input type="file" v-show="false" id="fileHelper" ref="fileHelper" @change="uploadFile">
+
+        <el-table
+            :data="result"
+            v-show="result&&result.length>0"
+            style="width: 100%">
+            <el-table-column
+                prop="url"
+                label="文件路径"
+                >
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 
 <script>
+import api from "../../../common/api/api.js"
+import {HOST} from "../../../common/api/api.js"
     export default {
         data() {
             return {
+                hasSelectedFile:false,
+                percentage:0,
+                result:[]
             }
         },
         computed:{
+            
         },
         methods: {
+            selectFile(){
+            　  var evt = document.createEvent("MouseEvents");  
+                evt.initEvent("click", true, true);  
+                this.$refs["fileHelper"].dispatchEvent(evt); 
+            },
+            uploadFile(e){
+                this.hasSelectedFile = true;
+                this.startProgress();
+                setTimeout(()=>{
+                    let data = ["/services/static/tttdel7/.DS_Store","/services/static/tttdel7/._.DS_Store","/services/static/tttdel7/hhh.txt","/services/static/tttdel7/._hhh.txt"]
+                    // api.uploadFile(e.srcElement.files[0]).then((data)=>{
+                    //     if(!data||data.length<0){
+                    //         this.$message.error("上传失败")
+                    //         this.hasSelectedFile = false;
+                    //     }
+                        let result = this.composeFile(data);
+                        this.result = result;
+                        this.percentage = 100;
+                    // })
+                },2000)
+            },
+            startProgress(){
+                if(this.percentage<90){
+                    this.percentage+=Math.ceil(Math.random()*10)
+                    setTimeout(()=>{
+                        this.startProgress();
+                    },300)
+                }
+            },
+            composeFile(arr){
+                let result = [];
+                arr.map((item)=>{
+                    if(item.indexOf(".")==0)
+                        return
+                    result.push({
+                        url:HOST+item.replace(/\/services/,'')
+                    })
+                })
+                return result
+            }
+        },
+        mounted(){
+            // setTimeout(()=>{
+            //     this.hasSelectedFile = true
+            // },2000)
         }
     }
 </script>
+<style lang="less" rel="stylesheet/less">
+    .file_container{
+
+        .file-uploader  {
+            border: 1px dashed #d9d9d9;
+            border-radius: 6px;
+            cursor: pointer;
+            position: relative;
+            overflow: hidden;
+            width:126px;
+            height: 126px;
+            margin-bottom:30px;
+        }
+        .file-uploader:hover {
+            border-color: #20a0ff;
+        }
+        .file-uploader-icon {
+            font-size: 28px;
+            color: #8c939d;
+            width: 126px;
+            height: 126px;
+            line-height: 126px;
+            text-align: center;
+        }
+        
+    }
+</style>
