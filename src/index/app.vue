0d734bbb4625dbce675010fd6a5cb34d7206d8d9
Leibown <leibown.leila@gmail.com>
2017-09-19 19:51:24 +0800

    Merge branch 'master' of https://github.com/MJSJ/mjsj-cms
    # Conflicts:
    #	src/common/api/api.js
    #	src/index/app.vue

--- src/index/app.vue
+++ src/index/app.vue
@@ -44,33 +44,33 @@
         //在加载后会自动执行
         mounted() {
             // 调试用
-             window._DATA={
-                 u:{
-                     id:100001,
-                     name:"旷宇",
-                     role:2
-                 }
-             }
-             api.login({
-                 userID:"100001",
-                 password:"ddd"
-             })
-             .then((data)=>{
-                 this.$store.dispatch('fetchUserList');
-                 this.$store.dispatch('fetchSubjectList');
-                 this.$store.dispatch('fetchTotalTopics', {status: 1});
-             })
+//             window._DATA={
+//                 u:{
+//                     id:100001,
+//                     name:"旷宇",
+//                     role:2
+//                 }
+//             }
+//             api.login({
+//                 userID:"100001",
+//                 password:"ddd"
+//             })
+//             .then((data)=>{
+//                 this.$store.dispatch('fetchUserList');
+//                 this.$store.dispatch('fetchSubjectList');
+//                 this.$store.dispatch('fetchTotalTopics', {status: 1});
+//             })
 
             /**
              * 正式环境调用
              * push 到master都应该把
              * 调试的注释了
              */
-//            if(window._DATA&&window._DATA.u){
-//                this.$store.dispatch('fetchUserList');
-//                this.$store.dispatch('fetchSubjectList');
-//                this.$store.dispatch('fetchTotalTopics', {status: 1});
-//            }
+            if(window._DATA&&window._DATA.u){
+                this.$store.dispatch('fetchUserList');
+                this.$store.dispatch('fetchSubjectList');
+                this.$store.dispatch('fetchTotalTopics', {status: 1});
+            }
 
 
         },
